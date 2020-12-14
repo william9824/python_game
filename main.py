40d14c80ff362f61c93b47741a4cd643628d2ea3
William Chung <31009775+william9824@users.noreply.github.com>
2020-12-15 04:04:34 +0900

    Need Collusion and Laser, then go to good

--- main.py
+++ main.py
@@ -39,15 +39,29 @@ class Ship:
         self.cool_down_counter = 0
 
     def draw(self,window):
-        # pygame.draw.rect(window,(255,0,0),(self.x, self.y, 50, 50))
+        # pygame.draw.rect(window,(255,0,0),(self.x, self.y, 50, 50))  * RED BOX FOR TEST *
         window.blit(self.ship_img, (self.x, self.y))
 
-    def get_height(self):
+    def get_height(self): # Get ship grid  * edge position *
         return self.ship_img.get_height()
 
-    def get_width(self):
+    def get_width(self): # Get ship grid * edge position *
         return self.ship_img.get_width()
 
+class Enemy(Ship):
+    COLOR_MAPPING = {
+        "RED": (RED_SHIP, RED_LASER),
+        "GREEN": (GREEN_SHIP, GREEN_LASER),
+        "BLUE" : (BLUE_SHIP, BLUE_LASER),
+    }
+
+    def __init__(self,x,y,color, health = 100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAPPING[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, vel):
+        self.y += vel
 
 
 # Player Ship
@@ -64,10 +78,19 @@ class Player(Ship):
 # Control flow - main function
 def main():
     run = True  # start
-    level = 1
-    lifes = 5
+    main_font = pygame.font.SysFont("comicsans", 45)
+    lost_font = pygame.font.SysFont("comicsans", 50)
+    lost = False
+    lost_count = 0
+    level = 0
+    lives = 5
     FPS = 60 # FPS, Game Speed
-    main_font = pygame.font.SysFont("comicsans",60)
+
+
+
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
 
     player_vel = 5 # Velocity
 
@@ -76,14 +99,21 @@ def main():
     clock = pygame.time.Clock()
 
     # redraw Window
-    def redraw_window():
+    def redraw_window(): # Rendering procession
         WIN.blit(BKG,(0,0))  # Coordinate Stuff : From left-top to right-bottom
         # draw text
-        lifes_label = main_font.render(f"life: {lifes}", 1, (255,255,255)) # F-string
+        lifes_label = main_font.render(f"lives: {lives}", 1, (255,255,255)) # F-string
         level_label = main_font.render(f"level: {level}", 1, (255,255,255)) # F-string
 
         WIN.blit(lifes_label,(10,10)) # POS( x+10, y+10 ) : Top-left
         WIN.blit(level_label,(WIDTH - level_label.get_width() - 10, 10)) # POS( TOP_RIGHT - 10 , 10 ) : Top-right
+
+        for enemy in enemies: # Enemy class inherits "Ship.draw()"
+            enemy.draw(WIN)
+
+        if lost:
+            lost_label = lost_font.render("Lost",1, (255,255,255))
+            WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2 , HEIGHT/2)) # Put it Center : Window Center - Label's Center
 
         player.draw(WIN)
 
@@ -94,6 +124,24 @@ def main():
     while run:  # start
         clock.tick(FPS)
         redraw_window()
+
+        if lives <= 0 or player.health <= 0: # Are you lost?
+            lost = True
+            lost_count += 1
+
+        if lost:
+           if lost_count > FPS * 5: # Frame Per Second: 1Sec = 60 Frame | FPS * n = nSec : Timer
+               run = False
+           else:
+                continue
+
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length): # Spawn Randomly instead of coming down at the same time
+                enemy = Enemy(random.randrange(80, WIDTH-100), random.randrange(-1500, - 100), random.choice(["RED", "GREEN", "BLUE"]) ) # x,y { 80 ~ WIDTH (WIDTH -100), (-1200 ~ -120 ) }
+                enemies.append(enemy)
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -110,6 +158,11 @@ def main():
         if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT : # Go down
             player.y += player_vel
 
+        for enemy in enemies[:]: # Copy Enemy list, So it doesnt affect "list: enemies" -- Not necessary but safer --
+            enemy.move(enemy_vel)
+            if enemy.y + enemy.get_height() > HEIGHT: # Enemies go outside the window
+                lives -= 1
+                enemies.remove(enemy) # Destroy object
 
 
 main()
